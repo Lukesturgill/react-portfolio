7a7e7eef6cd9ee14670aad1031bf9b6d4fcb72fd
Luke Sturgill <lukesturgill@gmail.com>
2022-06-15 04:11:06 -0400

    building out page intro

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -1,4 +1,5 @@
 import "./intro.css"
+import Me from "../../img/me.png" 
 
 const Intro = () => {
     return (
@@ -8,12 +9,22 @@ const Intro = () => {
                     <h2 className="intro-name">Hello, my name is</h2>
                     <h1 className="name">Luke Sturgill</h1>
                     <div className="intro-title">
-                        <div className="i-title-wrapper"></div>
+                        <div className="i-title-wrapper">
+                            <div className="i-title-item">Web Developer</div>
+                            <div className="i-title-item">Web Developer</div>
+                            <div className="i-title-item">Web Developer</div>
+                            <div className="i-title-item">Web Developer</div>
+                            <div className="i-title-item">Web Developer</div>
+                        </div>
                     </div>
+                    <p className="intro-disc">
+                       placeholder placeholder placeholder finish discription I'm an aspiring full stack web developer, with knowledge in HTML, CSS, JavaScript, SQL, React, Node.js, and more. PLaceholder PLaceholder etc 
+                    </p>
                 </div>
             </div>
             <div className="intro-right">right</div>
-            Intro placeholder
+            <div className="i-bg"></div>
+            <img src={Me} alt="Luke" className="i-img" />
         </div>
     )
 }
