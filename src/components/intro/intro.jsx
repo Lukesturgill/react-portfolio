5ceceafec347707f4e63497219364c11afd907ca
Luke Sturgill <lukesturgill@gmail.com>
2022-06-15 00:27:30 -0400

    initial set up/ basic code added

---
+++ src/components/intro/intro.jsx
@@ -0,0 +1,21 @@
+import "./intro.css"
+
+const Intro = () => {
+    return (
+        <div className="intro">
+            <div className="intro-left">
+                <div className="i-left-wrapper">
+                    <h2 className="intro-name">Hello, my name is</h2>
+                    <h1 className="name">Luke Sturgill</h1>
+                    <div className="intro-title">
+                        <div className="i-title-wrapper"></div>
+                    </div>
+                </div>
+            </div>
+            <div className="intro-right">right</div>
+            Intro placeholder
+        </div>
+    )
+}
+
+export default Intro
